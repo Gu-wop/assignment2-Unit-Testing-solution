9800b0cd78ab9129564c7893cdf4fce859361d30
Gu-wop <aronkkristjansson@gmail.com>
2026-02-08 22:03:21 +0000

    Update dateUtils.ts

--- src/dateUtils.ts
+++ src/dateUtils.ts
@@ -1,11 +1,11 @@
 import moment from "moment";
 import { DATE_UNIT_TYPES } from "./constants";
 
-export function getCurrentYear() {
+export function getCurrentYear(): number {
   return moment().year();
 }
 
-export function add(date: Date, amount: number, type = DATE_UNIT_TYPES.DAYS) {
+export function add(date: Date, amount: number, type: DATE_UNIT_TYPES = DATE_UNIT_TYPES.DAYS): Date {
   if (!(date instanceof Date) || isNaN(date.getTime())) {
     throw new Error('Invalid date provided');
   }
@@ -15,18 +15,18 @@ export function add(date: Date, amount: number, type = DATE_UNIT_TYPES.DAYS) {
   return moment(date).add(amount, type).toDate();
 }
 
-export function isWithinRange(date: Date, from: Date, to: Date) {
+export function isWithinRange(date: Date, from: Date, to: Date): boolean {
   if (moment(from).isAfter(to)) {
     throw new Error('Invalid range: from date must be before to date');
   }
   return moment(date).isBetween(from, to);
 }
 
-export function isDateBefore(date: Date, compareDate: Date) {
+export function isDateBefore(date: Date, compareDate: Date): boolean {
   return moment(date).isBefore(compareDate);
 }
 
-export function isSameDay(date: Date, compareDate: Date) {
+export function isSameDay(date: Date, compareDate: Date): boolean {
   return moment(date).isSame(compareDate, 'day');
 }
 
